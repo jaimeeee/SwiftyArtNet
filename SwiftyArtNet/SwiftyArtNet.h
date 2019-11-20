f4f130110978031dec80bb8674067109f2557d85
Jaime Zaragoza <hola@jaimeeee.com>
2019-11-20 18:18:28 +0100

    Initial Commit

---
+++ SwiftyArtNet/SwiftyArtNet.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyArtNet.h
+//  SwiftyArtNet
+//
+//  Created by Jaime on 20/11/2019.
+//  Copyright © 2019 Jaimeeee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyArtNet.
+FOUNDATION_EXPORT double SwiftyArtNetVersionNumber;
+
+//! Project version string for SwiftyArtNet.
+FOUNDATION_EXPORT const unsigned char SwiftyArtNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyArtNet/PublicHeader.h>
+
+
