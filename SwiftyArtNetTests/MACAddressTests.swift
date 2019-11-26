aaf4d8581b8aa4822c420f596352305aa11f119b
Jaime Zaragoza <hola@jaimeeee.com>
2019-11-26 17:02:47 +0100

    Verify MAC Address

--- SwiftyArtNetTests/MACAddressTests.swift
+++ SwiftyArtNetTests/MACAddressTests.swift
@@ -16,13 +16,26 @@ class MACAddressTests: XCTestCase {
         let correctMACAddress = MACAddress(string: "A1:A2:A3:B1:B2:B3")
         let emptyMACAddress = MACAddress(string: ":::::")
         let incompleteMACAddress = MACAddress(string: "A1:A2:A3:B1:B2")
+        let wrongMACAddress = MACAddress(string: "AAA:AAA:AAA:AAA:AAA:AAA")
         let invalidMACAddress = MACAddress(string: "what are you even trying?")
         
         // THEN
         XCTAssertNotNil(correctMACAddress)
         XCTAssertNil(incompleteMACAddress)
         XCTAssertNil(invalidMACAddress)
-        XCTAssertNotNil(emptyMACAddress)
+        XCTAssertNil(wrongMACAddress)
+        XCTAssertNil(emptyMACAddress)
+    }
+
+    func testMACAddressString() {
+        // GIVEN
+        let macAddress = MACAddress(string: "12:34:56:78:90:ab")
+
+        // GIVEN
+        XCTAssertNotNil(macAddress)
+
+        // THEN
+        XCTAssert(macAddress?.stringValue == "12:34:56:78:90:AB")
     }
 
 }
--- SwiftyArtNetTests/SwiftyArtNetTests.swift
+++ SwiftyArtNetTests/SwiftyArtNetTests.swift
@@ -13,22 +13,30 @@ class SwiftyArtNetTests: XCTestCase {
 
     private enum Messages: String, CaseIterable {
         case artPoll = "Art-Net\0\0 \0\u{0E}\u{02}\0"
+        case nonArtNet = "Foo\0"
+        case artPollLowerVersion = "Art-Net\0\0 \0\u{0D}\u{02}\0"
     }
 
     func testArtNetHeader() {
+        // GIVEN
         let artNet = SwiftyArtNet()
-        for message in Messages.allCases {
-            let artNetMessage = message.rawValue.data(using: .ascii)!
-            XCTAssert(artNet.isArtNetMessage(data: artNetMessage), "Message is not Art-Net")
-        }
+        let artNetMessage = Messages.artPoll.rawValue.data(using: .ascii)
+        let nonArtNetMessage = Messages.nonArtNet.rawValue.data(using: .ascii)
+
+        // THEN
+        XCTAssert(artNet.isArtNetMessage(data: artNetMessage!), "Message is not ArtNet")
+        XCTAssertFalse(artNet.isArtNetMessage(data: nonArtNetMessage!))
     }
     
     func testArtNetMinimumProtocolVersion() {
+        // GIVEN
         let artNet = SwiftyArtNet()
-        for message in Messages.allCases {
-            let artNetMessage = message.rawValue.data(using: .ascii)!
-            XCTAssert(artNet.isArtNetMinimumProtocolVersion(data: artNetMessage))
-        }
+        let artNetMessage = Messages.artPoll.rawValue.data(using: .ascii)!
+        let artNetLowerVersionMessage = Messages.artPollLowerVersion.rawValue.data(using: .ascii)!
+
+        // THEN
+        XCTAssert(artNet.isArtNetMinimumProtocolVersion(data: artNetMessage), "Message is not minimum protocol version")
+        XCTAssertFalse(artNet.isArtNetMinimumProtocolVersion(data: artNetLowerVersionMessage))
     }
 
 }
